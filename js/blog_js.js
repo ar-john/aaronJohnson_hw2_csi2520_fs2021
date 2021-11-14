a9def921f2f62b5c7e217c049dd26f9790a3ab05
aaron johnson <arjohnson@oakland.edu>
2021-11-14 15:19:52 -0500

    saving here, added some js to blog for comment section

---
+++ js/blog_js.js
@@ -0,0 +1,32 @@
+let comBtn = document.querySelector('#commentBtn');
+comBtn.addEventListener('click', runInputTextEvent);
+
+
+let comForm = document.querySelector('#commentForm');
+
+let txtarea = document.createElement("textarea");
+
+let inputText = document.querySelector('input[type="text"]');
+
+//inputText.addEventListener('keydown', runInputTextEvent); 
+
+function runInputTextEvent(e) {
+    console.log(`EVENT TYPE: ${e.type}`); // each time you type it will fire the event and increment counter 
+
+    //Grab what is being typed i.e. the value */
+    console.log(e.target.value);
+
+    //Take contnet and populate to DOM */
+    
+    comForm.innerHTML = `<textarea id="textArea">${e.target.value}</textarea>`;
+    let submitBtn = document.createElement("button");
+    submitBtn.innerHTML = "Submit Comment";
+    comForm.appendChild(submitBtn);
+}
+submitBtn.addEventListener('click', addComment);
+
+function addComment(e) {
+
+    
+
+}
