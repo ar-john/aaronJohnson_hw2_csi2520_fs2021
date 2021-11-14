fd424f296ea184fdaa0164ea3b135a2dd0e9787c
aaron johnson <arjohnson@oakland.edu>
2021-11-14 15:42:28 -0500

    committing to update vscode

--- js/blog_js.js
+++ js/blog_js.js
@@ -4,11 +4,13 @@ comBtn.addEventListener('click', runInputTextEvent);
 
 let comForm = document.querySelector('#commentForm');
 
-let txtarea = document.createElement("textarea");
+let txtarea = document.createElement("textArea");
 
 let inputText = document.querySelector('input[type="text"]');
 
 //inputText.addEventListener('keydown', runInputTextEvent); 
+let submitBtn = document.createElement("button");
+submitBtn.innerHTML = "Submit Comment";
 
 function runInputTextEvent(e) {
     console.log(`EVENT TYPE: ${e.type}`); // each time you type it will fire the event and increment counter 
@@ -19,14 +21,16 @@ function runInputTextEvent(e) {
     //Take contnet and populate to DOM */
     
     comForm.innerHTML = `<textarea id="textArea">${e.target.value}</textarea>`;
-    let submitBtn = document.createElement("button");
-    submitBtn.innerHTML = "Submit Comment";
+    // let submitBtn = document.createElement("button");
+    // submitBtn.innerHTML = "Submit Comment";
     comForm.appendChild(submitBtn);
 }
 submitBtn.addEventListener('click', addComment);
 
 function addComment(e) {
 
-    
+    let y = txtarea.document.value;
+    console.log(y);
+
 
 }
