4d974a2988da1338ad00f92094ad94ad07521af1
aaron johnson <arjohnson@oakland.edu>
2021-12-04 17:04:45 -0500

    Started working on express framework, next need heroku

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const ejs = require('ejs');
+
+//create express app
+const app = express();
+
+//initialize body parser midddleware to parse data sent by users
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+//initialize ejs middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+//routes
+app.get("/", (req,res) => {
+    res.render("index");
+});
+
+app.get("/my_resume", (req,res) => {
+    res.render("/html/my_resume");
+});
+
+app.get("/my_fun_facts", (req,res) => {
+    res.render("/html/my_fun_facts.ejs");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server is running on port 3000'));
