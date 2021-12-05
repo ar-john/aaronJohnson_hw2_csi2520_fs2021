57f91c1c5673b51961d6b4c54d00cab7ab3a4e0a
aaron johnson <arjohnson@oakland.edu>
2021-12-04 21:16:10 -0500

    set up express framework and routes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const ejs = require('ejs');
+const path = require("path");
 
 //create express app
 const app = express();
 
 //initialize body parser midddleware to parse data sent by users
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({extended: false }));
 
 //initialize ejs middleware
 app.set("view engine", "ejs");
@@ -17,12 +18,20 @@ app.get("/", (req,res) => {
     res.render("index");
 });
 
-app.get("/my_resume.ejs", (req,res) => {
-    res.render("/html/my_resume.ejs");
+app.get("/html/my_resume.ejs", (req,res) => {
+    res.render("./html/my_resume.ejs");
 });
 
-app.get("/my_fun_facts.ejs", (req,res) => {
-    res.render("/html/my_fun_facts.ejs");
+app.get("/html/my_fun_facts.ejs", (req,res) => {
+    res.render("./html/my_fun_facts.ejs");
+});
+
+app.get("/html/blog.ejs", (req,res) => {
+    res.render("./html/blog.ejs");
+});
+
+app.get("/index.ejs", (req,res) => {
+    res.render("index");
 });
 
 const PORT = process.env.PORT || 3000;
